73ce10c1c49101a08b45bd512ad3d395eb32527b
Tori <macbookpro@MacBook-Pro-MacBook.local>
2022-02-05 15:00:47 +0300

    Cname and test component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SimpleLinks/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SimpleLinks from './components/SimpleLinks.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SimpleLinks
   }
 }
 </script>
---
+++ src/components/SimpleLinks.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="links">
+    <div class="links__wrap">
+      <a class="" href="">Telegram</a>
+      <a class="" href="">WhatsApp</a>
+      <a class="" href="mailto:v.borozdina@gmail.com">Email</a>
+    </div>
+
+  </section>
+
+</template>
+
+<script>
+export default {
+  name: 'SimpleLinks',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+a{
+  display: block;
+  padding: 15px;
+  margin-bottom: 20px;
+  color: black;
+  border: 1px solid black;
+  transition: 0.3 all;
+  &:hover{
+    background-color: black;
+    color:white;
+    border: 1pxp solid blue;
+  }
+}
+</style>
